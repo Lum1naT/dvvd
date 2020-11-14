51b834644cea3ed333000309e8f0eb2ecd76cc91
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2020-11-14 11:18:18 +0100

    templates changes

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
